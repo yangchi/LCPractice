0647611711bd4bc581ef3e6406156950590616cc
Yang CHI <yangchi@cisco.com>
2014-02-06 22:28:52 -0800

    added Construct BT from traversal problems. Both MLE

---
+++ Construct_BT_Inorder_Postorder.cc
@@ -0,0 +1,37 @@
+//MLE
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ * int val;
+ * TreeNode *left;
+ * TreeNode *right;
+ * TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+	public:
+		TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
+
+			if(inorder.empty() || postorder.empty())
+				return NULL;
+			TreeNode *root = NULL;
+			if(inorder.size() == 1 && postorder.size() == 1)
+			{
+				root = new TreeNode(inorder[0]);
+				return root;
+			}
+			int rootval = *(postorder.end()-1);
+			root = new TreeNode(rootval);
+
+			vector<int>::iterator root_inorder_pos = find(inorder.begin(), inorder.end(), rootval);
+			int leftsize = root_inorder_pos - inorder.begin();
+			vector<int> inorder_left(inorder.begin(), root_inorder_pos);
+			vector<int> inorder_right(root_inorder_pos+1, inorder.end());
+			vector<int> postorder_left(postorder.begin(), postorder.begin() + leftsize);
+			vector<int> postorder_right(postorder.begin()+leftsize, postorder.end()-1);
+			root->left = buildTree(inorder_left, postorder_left);
+			root->right = buildTree(inorder_right, postorder_right);
+			return root;
+		}
+};
---
+++ Construct_BT_Preorder_Inorder.cc
@@ -0,0 +1,34 @@
+//MLE
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ * int val;
+ * TreeNode *left;
+ * TreeNode *right;
+ * TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+	public:
+		TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+			if(preorder.empty() || inorder.empty())
+				return NULL;
+			TreeNode *root;
+			if(preorder.size() == 1 && inorder.size() == 1)
+			{
+				root = new TreeNode(inorder[0]);
+				return root;
+			}
+			int rootval = preorder[0];
+			root = new TreeNode(rootval);
+			int leftsize = find(inorder.begin(), inorder.end(), rootval) - inorder.begin();
+			vector<int> inorder_left(inorder.begin(), inorder.begin() + leftsize);
+			vector<int> inorder_right(inorder.begin() + leftsize + 1, inorder.end());
+			vector<int> preorder_left(preorder.begin() + 1, preorder.begin() + 1 + leftsize);
+			vector<int> preorder_right(preorder.begin() + 1 + leftsize, preorder.end());
+			root->left = buildTree(preorder_left, inorder_left);
+			root->right = buildTree(preorder_right, inorder_right);
+			return root;
+		}
+};
