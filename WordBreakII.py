2c06f5d57f6c33bf37f086304a133d786797e93b
Yang CHI <yangchi@cisco.com>
2014-09-17 23:51:15 -0700

    added a better and probably correct Word Break II that still overtimes

--- WordBreakII.py
+++ WordBreakII.py
@@ -3,32 +3,26 @@ class Solution:
     # @param dict, a set of string
     # @return a list of strings
     def wordBreak(self, s, dict):
-        sentences = {}
-        for letter in s:
-            if letter in dict:
-                if letter in sentences.keys():
-                    sentences[letter].append((letter))
-                else:
-                    sentences[letter] = [(letter)]
-        for length in range(2, len(s)+1):
-            for start in range(0, len(s)):
-                end = start + length
-                if end <= len(s):
-                    for mid in range(start, end + 1):
-                        if s[start:mid+1] in sentences.keys() and s[mid + 1:end] in sentences.keys():
-                            if s[start:end] in sentences.keys():
-                                sentences[s[start:end]].append((s[start:mid+1], s[mid+1:end]))
-                            else:
-                                sentences[s[start:end]] = [(s[start:mid], s[mid + 1:end])]
-        if s not in sentences.keys():
-            return []
-        else:
-            ret = []
-            for combo in sentences[s]:
-                str_repr = ""
-                for word in combo:
-                    str_repr += word + " "
-                ret.append(str_repr[:-1])
-            return ret
-
-        
+        if len(s) == 0:
+            if "" in dict:
+                return [""]
+            else:
+                return []
+        booleans = [False for i in range(len(s))]
+        sentences = [set() for i in range(len(s))]
+        if s[0] in dict:
+            booleans[0] = True
+            sentences[0].add(s[0])
+        for pos in range(1, len(s)):
+            if s[:pos+1] in dict:
+                booleans[pos] = True
+                sentences[pos].add(s[:pos+1])
+            for sub in range(0, pos):
+                if booleans[sub] and s[sub+1:pos+1] in dict:
+                    booleans[pos] = True
+                    for substr in sentences[sub]:
+                        if substr != "":
+                            sentences[pos].add(substr + " " + s[sub+1:pos+1])
+                        else:
+                            sentences[pos].add(s[sub+1 : pos+1])
+        return list(sentences[len(s)-1])
