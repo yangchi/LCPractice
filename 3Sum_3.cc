95a517d8a1288fa025c81b79bb0a64859b888448
Yang CHI <yangchi@cisco.com>
2014-10-29 00:46:19 -0700

    I have no idea why 3SUM_3 Exceeds time limit on LC

--- 3Sum_3.cc
+++ 3Sum_3.cc
@@ -1,3 +1,9 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
 class Solution {
     public:
         vector<vector<int> > threeSum(vector<int> &num) {
@@ -13,6 +19,12 @@ class Solution {
                         newresult.push_back(num[index_left++]);
                         newresult.push_back(num[index_right--]);
                         result.push_back(newresult);
+                        while(index_left < index_right && num[index_left + 1] == num[index_left]) {
+                            index_left++;
+                        }
+                        while(index_left < index_right && num[index_right - 1] == num[index_right]) {
+                            index_right--;
+                        }
                     } else if (num[i] + num[index_left] + num[index_right] > 0) {
                         index_right--;
                     } else {
@@ -22,4 +34,17 @@ class Solution {
             }
             result.erase(unique(result.begin(), result.end()), result.end());
             return result;
+        }
+};
 
+int main()
+{
+    Solution sol;
+    vector<int> input {1, 0, -1};
+    vector<vector<int>> result = sol.threeSum(input);
+    for(auto vec: result) {
+        for(auto elem: vec) {
+             cout << elem << endl;
+        }
+    }
+}
