4bc20e1ec917b982492df7534b8f169abfcd8246
Yang CHI <yangchi@cisco.com>
2014-11-18 18:03:31 -0800

    take anagram again. saved new AC solution. but seems worse than previous one

--- anagram.cc
+++ anagram.cc
@@ -1,23 +1,21 @@
 class Solution {
-    public:
-        vector<string> anagrams(vector<string> &strs) {
-            // Start typing your C/C++ solution below
-            // DO NOT write int main() function
-            map<string, vector<string> > mappings;
-            for(auto str: strs)
-            {
-                string temp(str);
-                sort(temp.begin(), temp.end());
-                mappings[temp].push_back(str);
+public:
+    vector<string> anagrams(vector<string> &strs) {
+        vector<string> result;
+        map<string, vector<string>> sorted_to_raw;
+        for(auto str : strs) {
+            string temp{str};
+            sort(temp.begin(), temp.end());
+            if(sorted_to_raw.find(temp) == sorted_to_raw.end()) {
+                sorted_to_raw[temp] = vector<string>();
             }
-            vector<string> result;
-            for(auto mapping: mappings)
-            {
-                if(mapping.second.size() > 1)
-                {
-                    copy(mapping.second.begin(), mapping.second.end(), back_inserter(result));
-                }
-            }
-            return result;
+            sorted_to_raw[temp].push_back(str);
         }
+        for(auto kv : sorted_to_raw) {
+            if(kv.second.size() > 1) {
+                result.insert(result.end(), kv.second.begin(), kv.second.end());
+            }
+        }
+        return result;
+    }
 };
