e2c3d867f24984c54617d4014df4606e87201626
Yang CHI <yangchi@cisco.com>
2014-11-19 21:00:42 -0800

    2nd version of Add Two Numbers

--- AddingTwoNumbers.cc
+++ AddingTwoNumbers.cc
@@ -1,57 +1,62 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- * int val;
- * ListNode *next;
- * ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
 class Solution {
-    public:
-        ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-            // Start typing your C/C++ solution below
-            // DO NOT write int main() function
-            int carry = 0;
-            int current = 0;
-            ListNode *result = NULL, *restore = NULL;
-            ListNode *l1pos = l1, *l2pos = l2;
-            while(l1pos && l2pos){
-                current = l1pos->val + l2pos->val + carry;
-                carry = current / 10;
-                current %= 10;
-                ListNode *tempNode = new ListNode(current);
-                if(result)
-                    result->next = tempNode;
-                else
-                    restore = tempNode;
-                result = tempNode;
-                l1pos = l1pos->next;
-                l2pos = l2pos->next;
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode *result = NULL;
+        ListNode *result_current = result;
+        ListNode *l1_current = l1, *l2_current = l2;
+        int carry = 0;
+        int current_result = 0;
+        while(l1_current && l2_current) {
+            current_result = carry + l1_current->val + l2_current->val;
+            carry = current_result / 10;
+            current_result %= 10;
+            if(!result) {
+                result = new ListNode(current_result);
+                result_current = result;
+            } else {
+                result_current->next = new ListNode(current_result);
+                result_current = result_current->next;
             }
-            while(l1pos){
-                ListNode *tempNode = new ListNode((carry + l1pos->val)%10);
-                carry = (carry + l1pos->val) / 10;
-                if(result)
-                    result->next = tempNode;
-                else
-                    restore = tempNode;
-                result = tempNode;
-                l1pos = l1pos->next;
-            }
-            while(l2pos){
-                ListNode *tempNode = new ListNode((carry + l2pos->val)%10);
-                carry = (carry + l2pos->val) / 10;
-                if(result)
-                    result->next = tempNode;
-                else
-                    restore = tempNode;
-                result = tempNode;
-                l2pos = l2pos->next;
-            }
-            if(carry){
-                ListNode *tempNode = new ListNode(carry);
-                result->next = tempNode;
-            }
-            return restore;
+            l1_current = l1_current->next;
+            l2_current = l2_current->next;
         }
+        while(l1_current) {
+            current_result = l1_current->val + carry;
+            carry = current_result / 10;
+            current_result %= 10;
+            if(!result) {
+                result = new ListNode(current_result);
+                result_current = result;
+            } else {
+                result_current->next = new ListNode(current_result);
+                result_current = result_current->next;
+            }
+            l1_current = l1_current->next;
+        }
+        while(l2_current) {
+            current_result = l2_current->val + carry;
+            carry = current_result / 10;
+            current_result %= 10;
+            if(!result) {
+                result = new ListNode(current_result);
+                result_current = result;
+            } else {
+                result_current->next = new ListNode(current_result);
+                result_current = result_current->next;
+            }
+            l2_current = l2_current->next;
+        }
+        if(carry == 1) {
+            result_current->next = new ListNode(1);
+        }
+        return result;
+    }
 };
