4ace8b688b7ea272e4cd62316c858180b6c6fa0b
Yang CHI <yangchi@cisco.com>
2014-11-22 21:23:47 -0800

    better version of Contain with most Water

--- ContainerWithMostWater.cc
+++ ContainerWithMostWater.cc
@@ -1,24 +1,17 @@
 class Solution {
     public:
         int maxArea(vector<int> &height) {
-            // Start typing your C/C++ solution below
-            // DO NOT write int main() function
-            int max = 0;
-            for(int i = 0; i < height.size();)
-            {
-                for(int j = height.size()-1; j >=0;)
-                {
-                    if(i == j)
-                        return max;
-                    int area = 0;
-                    if(height[i] < height[j])
-                        area = (j-i)*height[i++];
-                    else
-                        area = (j-i)*height[j--];
-                    if(area > max)
-                        max = area;
-                }
+            int maxarea = 0;
+            int left = 0, right = height.size() - 1;
+            while(left < right) {
+                int area = (right - left) * min(height[left], height[right]);
+                if (maxarea < area)
+                    maxarea = area;
+                if(height[left] < height[right])
+                    left++;
+                else
+                    right--;
             }
-            return max;
+            return maxarea;
         }
 };
