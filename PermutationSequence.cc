a6c6ca2a28e711028a274c300deb018c885bb0e1
Yang CHI <yangchi@cisco.com>
2015-01-02 17:15:46 -0800

    a slightly better version of Permutation Sequence

--- PermutationSequence.cc
+++ PermutationSequence.cc
@@ -1,28 +1,23 @@
 class Solution {
-    public:
-        string getPermutation(int n, int k) {
-            // Start typing your C/C++ solution below
-            // DO NOT write int main() function
-            vector<int> facs(n, 1); //factorials: 0, 1!, ..., (n-1)!
-            for(int i = 1; i < facs.size(); i++){
-                facs[i] *= facs[i-1] * i;
-            }
-            k--;
-            string output;
-            vector<int> num_each_fac(n, 0);
-            for(int i = n; i > 0; i--){
-                if(k >= facs[i-1]){
-                    num_each_fac[i-1] = k/facs[i-1];
-                    k = k % facs[i-1];
-                }
-            }
-            vector<int> digs(n);
-            for(int i = 0; i < n; i++)
-                digs[i] = i+1;
-            for(int i = n; i > 0; i--){
-                output.append(to_string(digs[num_each_fac[i-1]]));
-                digs.erase(digs.begin()+num_each_fac[i-1]);
-            }
-            return output;
+public:
+    string getPermutation(int n, int k) {
+        vector<int> factorials(2, 1);
+        for(int i = 2; i < n; i++) {
+            factorials.push_back(factorials[factorials.size() - 1] * i);
         }
+        vector<int> num_of_factorials(n, 0);
+        int temp = k - 1;
+        string result;
+        vector<int> digits(n);
+        for(int i = 0; i < n; i++) {
+            digits[i] = i + 1;
+        }
+        for(int i = n; i > 0; i--) {
+            div_t quot_rem = div(temp, factorials[i - 1]);
+            result += to_string(digits[quot_rem.quot]);
+            digits.erase(digits.begin() + quot_rem.quot);
+            temp = quot_rem.rem;
+        }
+        return result;
+    }
 };
