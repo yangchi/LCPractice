4722a013c4989c625a3d5be0aff55859c1f2fd21
Yang CHI <yangchi@cisco.com>
2015-01-06 20:35:25 -0800

    Passed OJ: Construct BST from Inorder Postorder

--- Construct_BT_Inorder_Postorder.cc
+++ Construct_BT_Inorder_Postorder.cc
@@ -1,37 +1,43 @@
-//MLE
-
 /**
- * Definition for binary tree
- * struct TreeNode {
- * int val;
- * TreeNode *left;
- * TreeNode *right;
- * TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+* Definition for binary tree
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+
+typedef vector<int>::iterator iter;
+
 class Solution {
-    public:
-        TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
+public:
+    TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
+        return buildTreeRecur(inorder.begin(), inorder.end(), postorder.begin(), postorder.end());
+    }
 
-            if(inorder.empty() || postorder.empty())
-                return NULL;
-            TreeNode *root = NULL;
-            if(inorder.size() == 1 && postorder.size() == 1)
-            {
-                root = new TreeNode(inorder[0]);
-                return root;
-            }
-            int rootval = *(postorder.end()-1);
-            root = new TreeNode(rootval);
-
-            vector<int>::iterator root_inorder_pos = find(inorder.begin(), inorder.end(), rootval);
-            int leftsize = root_inorder_pos - inorder.begin();
-            vector<int> inorder_left(inorder.begin(), root_inorder_pos);
-            vector<int> inorder_right(root_inorder_pos+1, inorder.end());
-            vector<int> postorder_left(postorder.begin(), postorder.begin() + leftsize);
-            vector<int> postorder_right(postorder.begin()+leftsize, postorder.end()-1);
-            root->left = buildTree(inorder_left, postorder_left);
-            root->right = buildTree(inorder_right, postorder_right);
-            return root;
+    TreeNode *buildTreeRecur(iter inorder_begin, iter inorder_end, iter postorder_begin, iter postorder_end) {
+        if (inorder_begin == inorder_end || postorder_begin == postorder_end) {
+            return NULL;
         }
+
+        int tree_size = inorder_end - inorder_begin;
+        if (postorder_end - postorder_begin != tree_size) {
+            return NULL;
+        }
+
+        TreeNode *root = new TreeNode(*(postorder_end - 1));
+        iter root_iter_inorder = find(inorder_begin, inorder_end, root->val);
+        iter inorder_left_begin = inorder_begin;
+        iter inorder_left_end = root_iter_inorder;
+        iter inorder_right_begin = root_iter_inorder + 1;
+        iter inorder_right_end = inorder_end;
+        iter postorder_left_begin = postorder_begin;
+        iter postorder_left_end = postorder_begin + (inorder_left_end - inorder_left_begin);
+        iter postorder_right_begin = postorder_left_end;
+        iter postorder_right_end = postorder_end - 1;
+        root->left = buildTreeRecur(inorder_left_begin, inorder_left_end, postorder_left_begin, postorder_left_end);
+        root->right = buildTreeRecur(inorder_right_begin, inorder_right_end, postorder_right_begin, postorder_right_end);
+        return root;
+    }
 };
