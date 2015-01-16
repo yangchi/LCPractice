54a495fcaa42989f7214365a62be290c7b09f55d
Yang CHI <yangchi@cisco.com>
2015-01-16 10:55:07 -0800

    slightly better version of Maximum Subarray

--- MaximumSubarray.cc
+++ MaximumSubarray.cc
@@ -1,23 +1,20 @@
 class Solution {
     public:
         int maxSubArray(int A[], int n) {
-            int max_ends_here[n];
+            int running[n];
             for(int i = 0; i < n; i++) {
-                if (i == 0) {
-                    max_ends_here[i] = A[i];
-                } else {
-                    if (max_ends_here[i - 1] < 0) {
-                        max_ends_here[i] = A[i];
-                    } else {
-                        max_ends_here[i] = A[i] + max_ends_here[i - 1];
-                    }
+                running[i] = A[i];
+            }
+
+            int max_running = INT_MIN;
+            for(int i = 0; i < n; i++) {
+                if (i != 0 && running[i - 1] > 0) {
+                    running[i] += running[i - 1];
+                }
+                if (max_running < running[i]) {
+                    max_running = running[i];
                 }
             }
-            int max = INT_MIN;
-            for(int i = 0; i < n; i++) {
-                if(max < max_ends_here[i])
-                    max = max_ends_here[i];
-            }
-            return max;
+            return max_running;
         }
 };
