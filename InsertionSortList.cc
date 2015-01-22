d453a7ddae76f345c90616661acd66470cfa0522
Yang CHI <yangchi@cisco.com>
2015-01-21 22:15:45 -0800

    better version of Insertion Sort List

--- InsertionSortList.cc
+++ InsertionSortList.cc
@@ -1,53 +1,41 @@
 /**
- ** Definition for singly-linked list.
- ** struct ListNode {
- **     int val;
- **     ListNode *next;
- **     ListNode(int x) : val(x), next(NULL) {}
- ** };
- **/
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
 class Solution {
-    public:
-        ListNode *insertionSortList(ListNode *head) {
-            if (!head || !head->next) {
-                return head;
-            }
-            ListNode *pre_current = head;
-            ListNode *current = head->next;
-            ListNode *gun = head;
-            while (current) {
-                while(gun->next && gun->val <= current->val && gun->next->val <= current->val && gun->next != current) {
-                    gun = gun->next;
-                }
-                if (!gun->next) {
-                    /*gun doesn't have next. Should be all done. */
-                    break;
-                }
-                else if (gun->val > current->val) { 
-                    /* gun->val already bigger than current->val, insert current before gun.
-                     * this also means gun never moved. So gun = head
-                     */
-                    pre_current->next = current->next;
-                    current->next = gun;
-                    head = current;
-                    current = pre_current->next;
-                    gun = head;
-                } else if (gun->next == current) {
-                    /* Gun and current adjacent. In this case, if gun->val > current-> value, already cover.
-                     * Other wise, just move current and pre_current
-                     */
-                    pre_current = current;
-                    current = current->next;
-                    gun = head;
-                } else if(gun->next->val > current->val) {
-                    /* Gun and current not adjacent. Current should be moved between gun and gun->next */
-                    pre_current->next = current->next;
-                    current->next = gun->next;
-                    gun->next = current;
-                    current = pre_current->next;
-                    gun = head;
-                }
-            }
+public:
+    ListNode *insertionSortList(ListNode *head) {
+        if (!head || !head->next) {
             return head;
         }
+
+        ListNode *moving = head, *current_end = head;
+        while(current_end->next) {
+            ListNode *current = current_end->next;
+            while(moving != current_end && moving->val <= current->val && moving->next->val <= current->val) {
+                moving = moving->next;
+            }
+
+            if (moving->val > current->val) {
+                current_end->next = current->next;
+                current->next = moving;
+                head = current;
+                moving = head;
+            } else if (moving == current_end) {
+                current_end = current;
+                moving = head;
+            } else {
+                current_end->next = current->next;
+                current->next = moving->next;
+                moving->next = current;
+                moving = head;
+            }
+        }
+
+        return head;
+    }
 };
