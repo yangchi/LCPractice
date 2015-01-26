251f81ca1550778fd68d1519b0b593420fe879b1
Yang CHI <yangchi@cisco.com>
2015-01-25 22:30:27 -0800

    easier to understand version of Interleaving String

--- InterleavingString_DP.cc
+++ InterleavingString_DP.cc
@@ -1,38 +1,38 @@
-#include <iostream>
-
-using namespace std;
-
-
 class Solution {
     public:
         bool isInterleave(string s1, string s2, string s3) {
-            if(s1.length() + s2.length() != s3.length()) {
+            if (s1.size() + s2.size() != s3.size()) {
                 return false;
             }
-            bool interleave[s1.length() + 1][s2.length() + 1];
-            for(int i = 0; i < s1.length() + 1; i++)
-                for(int j = 0; j < s2.length() + 1; j++)
-                    interleave[i][j] = false;
-            interleave[0][0] = true;
-            int ind1 = 0, ind2 = 0, ind3 = 0;
-            for(ind1 = 0; ind1 < s1.length() + 1; ind1++) {
-                for(ind2 = 0; ind2 < s2.length() + 1; ind2++) {
-                    if(s1[ind1] == s3[ind1 + ind2]) {
-                        interleave[ind1 + 1][ind2] = interleave[ind1 + 1][ind2] || interleave[ind1][ind2];
-                    }
-                    if(s2[ind2] == s3[ind1 + ind2]) {
-                        interleave[ind1][ind2 + 1] = interleave[ind1][ind2 + 1] || interleave[ind1][ind2];
-                    }
+
+            /*
+             * dp[i][j] means if the first (i + j) chars in s3 is a cmobo of first i in s1 and first j in s2
+             *
+             * Note first i chars in s1 are: s1[0 ... i - 1]
+             */
+            int dp[s1.size() + 1][s2.size()  + 1];
+            for(int i = 0; i <= s1.size(); i++) {
+                for(int j = 0; j <= s2.size(); j++) {
+                    dp[i][j] = false;
                 }
             }
-            return interleave[s1.length()][s2.length()];
+
+            dp[0][0] = true;
+            for(int i = 0; i < s1.size(); i++) {
+                dp[i + 1][0] = s1[i] == s3[i];
+            }
+
+            for(int j = 0; j < s2.size(); j++) {
+                dp[0][j + 1] = s2[j] == s3[j];
+            }
+
+            for(int i = 1; i < s1.size() + 1; i++) {
+                for(int j = 1; j < s2.size() + 1; j++) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j] && s1[i - 1] == s3[i + j - 1];
+                    dp[i][j] = dp[i][j] || dp[i][j - 1] && s2[j - 1] == s3[i + j - 1];
+                }
+            }
+
+            return dp[s1.size()][s2.size()];
         }
 };
-
-int main() {
-    Solution sol;
-    cout << "Answer: " << sol.isInterleave("aabaac", "aadaaeaaf", "aadaaeaabaafaac") << endl;
-
-}
-
-
