85945001b8209aa5d3cf1f30ce51a2aa917e9129
Yang CHI <yangchi@cisco.com>
2015-01-25 21:54:10 -0800

    added slightly better Longest Consecutive Subseq

--- LongestConsecutiveSubsequence.cc
+++ LongestConsecutiveSubsequence.cc
@@ -2,39 +2,31 @@ class Solution {
     public:
         int longestConsecutive(vector<int> &num) {
             set<int> num_set(num.begin(), num.end());
-            int maxcluster = 0;
-            for(auto number: num_set)
-                maxcluster = max(maxcluster, updown(num_set, number));
-            return maxcluster;
+            int longest = 0;
+            while (!num_set.empty()) {
+                int curr = *num_set.begin();
+                num_set.erase(curr);
+                int curr_length = left_right_consecutive(num_set, curr);
+                if (curr_length > longest) {
+                    longest = curr_length;
+                }
+            }
+
+            return longest;
         }
 
-        int updown(set<int> & numset, int number)
-        {
-            int count = 0;
-            if(numset.find(number) != numset.end())
-            {
-                numset.erase(number);
-                count++;
-                int up = updowncount(numset, number+1, true);
-                int down = updowncount(numset, number-1, false);
-                count += up + down;
+        int left_right_consecutive(set<int> & numset, int element) {
+            int length = 1;
+            if (numset.find(element + 1) != numset.end()) {
+                numset.erase(element + 1);
+                length += left_right_consecutive(numset, element + 1);
             }
-            return count;
-        }
 
-        int updowncount(set<int> & numset, int number, bool up)
-        {
-            int count = 0;
-            while(numset.find(number) != numset.end())
-            {
-                count++;
-                numset.erase(number);
-                if(up)
-                    number++;
-                else
-                    number--;
+            if (numset.find(element - 1) != numset.end()) {
+                numset.erase(element - 1);
+                length += left_right_consecutive(numset, element - 1);
             }
-            return count;
+
+            return length;
         }
 };
-
