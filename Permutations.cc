e9106ee4d5385d71d82b26dc3e3fa4a589e95cfd
Yang CHI <yangchi@cisco.com>
2015-01-26 21:29:57 -0800

    a waaaaay better Permutations

--- Permutations.cc
+++ Permutations.cc
@@ -1,34 +1,26 @@
 class Solution {
-    public:
-        vector<vector<int> > permute(vector<int> &num) {
-            // Start typing your C/C++ solution below
-            // DO NOT write int main() function
-            map<int, bool> used;
-            vector<int>::const_iterator iter;
-            for(iter = num.begin(); iter != num.end(); iter++){
-                used.insert(make_pair(*iter, false));
-            }
-            vector<int> temp;
-            vector<vector<int> > result;
-            permutation(0, used, num, temp, result);
-            return result;
+public:
+    vector<vector<int> > permute(vector<int> &num) {
+        vector<vector<int>> result;
+        permuteDFS(num, 0, result);
+        return result;
+    }
+
+    void permuteDFS(vector<int> & num, int position, vector<vector<int>> & result) {
+        if (position == num.size()) {
+            result.push_back(num);
         }
 
-        void permutation(int position, map<int, bool> & used, vector<int> & num, vector<int> & current, vector<vector<int> > & result){
-            if(position == num.size()){
-                result.push_back(current);
-                //current.clear();
-                return;
-            }
-            vector<int>::const_iterator iter;
-            for(iter = num.begin(); iter != num.end(); iter++){
-                if(!used.find(*iter)->second){
-                    current.push_back(*iter);
-                    used.find(*iter)->second = true;
-                    permutation(position+1, used, num, current, result);
-                    used.find(*iter)->second = false;
-                    current.pop_back();
-                }
-            }
+        for(int i = position; i < num.size(); i++) {
+            swap(num, position, i);
+            permuteDFS(num, position + 1, result);
+            swap(num, position, i);
         }
+    }
+
+    void swap(vector<int> & num, int first, int second) {
+        int temp = num[first];
+        num[first] = num[second];
+        num[second] = temp;
+    }
 };
