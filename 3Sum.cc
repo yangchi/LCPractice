342b19b34918af51b88450145a074fac5a4f3e6f
Yang CHI <yangchi@cisco.com>
2015-01-26 20:17:25 -0800

    slightly better 3SUM

--- 3Sum.cc
+++ 3Sum.cc
@@ -1,52 +1,30 @@
-#include <vector>
-#include <algorithm>
-#include <iostream>
-
-using namespace std;
-
 class Solution {
 public:
     vector<vector<int> > threeSum(vector<int> &num) {
         sort(num.begin(), num.end());
-        vector<vector<int>> results;
-        for(int i = 0; i < num.size(); i++) {
-            if (i > 0 && num[i] == num[i - 1]) {
+        set<vector<int>> result;
+        for(int first = 0; first < num.size(); first++) {
+            if (first > 0 && num[first] == num[first - 1]) {
                 continue;
             }
-            int left = i + 1;
-            int right = num.size() - 1;
-            while(left < right) {
-                if (num[left] + num[right] + num[i] == 0) {
-                    results.push_back(vector<int>({num[i], num[left], num[right]}));
-                    while(left + 1 < right && num[left + 1] == num[left]) {
-                        left++;
-                    }
-                    left++;
-                    while(right - 1 > left && num[right - 1] == num[right]) {
-                        right--;
-                    }
-                    right--;
-                } else if (num[left] + num[right] + num[i] < 0) {
-                    left++;
+            int second = first + 1, third = num.size() - 1;
+            while(second < third) {
+                int current_sum = num[first] + num[second] + num[third];
+                if (current_sum == 0) {
+                    result.insert(vector<int>({num[first], num[second], num[third]}));
+                    do {
+                        second++;
+                    } while (num[second - 1] == num[second]);
+                    do {
+                        third--;
+                    } while (num[third + 1] == num[third]);
+                } else if (current_sum > 0) {
+                    third--;
                 } else {
-                    right--;
+                    second++;
                 }
-
             }
         }
-        results.erase(unique(results.begin(), results.end()), results.end());
-        return results;
+        return vector<vector<int>>(result.begin(), result.end());
     }
 };
-
-int main()
-{
-    Solution sol;
-    vector<int> input {-1, -1, -1, 0, 1, 1};
-    vector<vector<int>> result = sol.threeSum(input);
-    for(auto vec: result) {
-        for(auto elem: vec) {
-             cout << elem << endl;
-        }
-    }
-}
