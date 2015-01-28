03ddf4b87a20efa52a03d2edd42723b9a9ef0b32
Yang CHI <yangchi@cisco.com>
2015-01-27 16:50:46 -0800

    removed the old Restore IP Addr

--- RestoreIPAddresses.cc
+++ RestoreIPAddresses.cc
@@ -1,58 +1,69 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
 class Solution {
 public:
     vector<string> restoreIpAddresses(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
         vector<string> result;
-        splitter(s, 4, result);
+        string current = "";
+        splitToSections(s, 4, result, current);
         return result;
-    }    
+    }
 
-    bool splitter(string s, int limit, vector<string> & result) {
-        if(!limit || limit > 0 && s.empty())
+    bool legitStr(string s) {
+        if (s.empty()) {
             return false;
-        int number;
-        if(limit == 1) {
-            if(s[0] == '0' && s.size() > 1)
-                return false;
-            number = atoi(s.c_str());
-            if(number <= 255) {
-                vector<string>::iterator iter;
-                for(iter = result.begin(); iter != result.end(); iter++)
-                    *iter += s;
-                return true;
-            }
-            else
-                return false;
         }
-        bool workable = false;
-        vector<string> results;
-        for(int i = 1; i <=3 && i <= s.size(); i++) {
-            if(s[0] == '0' && i > 1)
-                continue;
-            vector<string> newresult(result);
-            number = atoi(s.substr(0, i).c_str());
-            if(number <= 255) {
-                if(limit == 4) {
-                    newresult.push_back(s.substr(0, i)+'.');
+
+        if (s.size() > 1 && s[0] == '0' && s[1] == '0') {
+            return false;
+        }
+
+        int num = stoi(s);
+        if (s[0] == '0' && num > 0) {
+            return false;
+        }
+        return num >= 0 && num <= 255;
+    }
+
+    void splitToSections(string s, int num_sections, vector<string> & result, string current) {
+        if (s.size() < num_sections || s.size() > 3 * num_sections) {
+            return;
+        }
+        if (num_sections == 1) {
+            if (legitStr(s)) {
+                current += "." + s;
+                result.push_back(current);
+            } else {
+                return;
+            }
+        }
+
+        for (int i = 1; i <= min((int)s.size() - 1, 3); i++) {
+            string substr = s.substr(0, i);
+            if (legitStr(substr)) {
+                string new_current = "";
+                if (current.empty()) {
+                    new_current = substr;
                 } else {
-                    vector<string>::iterator iter;
-                    for(iter = newresult.begin(); iter != newresult.end(); iter++) {
-                        *iter += s.substr(0, i) + ".";
-                    }
+                    new_current = current + "." + substr;
                 }
-                if(splitter(s.substr(i), limit-1, newresult)) {
-                    workable = true;
-                    copy(newresult.begin(), newresult.end(), back_inserter(results));
-                }
+                string new_s = s.substr(i);
+                splitToSections(new_s, num_sections - 1, result, new_current);
             }
-        }
-        if(workable) {
-            result = results;
-            return true;
-        }
-        else {
-            return false;
         }
     }
 };
+
+int main()
+{
+    Solution sol;
+    vector<string> result = sol.restoreIpAddresses("255255255255");
+    for(auto ip : result) {
+        cout << "Legit IP: " << ip << endl;
+    }
+
+}
