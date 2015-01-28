565a2cbecad2f3edafac1b0c0ed120ce19508f55
Yang CHI <yangchi@cisco.com>
2015-01-27 22:08:43 -0800

    Way better version of DEcode Ways

--- DecodeWays.cc
+++ DecodeWays.cc
@@ -1,42 +1,49 @@
-
 class Solution {
 public:
     int numDecodings(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        if (s.empty()) {
+            return 0;
+        }
         vector<int> ways(s.size(), 0);
-        if(s.empty())
-            return 0;
-        if(s.size() == 1 && s == "0")
-            return 0;
-        if(s[s.size()-1] == '0')
-            ways[s.size()-1] = 0;
-        else
-            ways[s.size()-1] = 1;
-        if(s.size() > 1)
-        {
-            if(s[s.size()-1] == '0' && s[s.size()-2] > '2')
-                return 0;
-            if(s[s.size()-2] == '0')
-                ways[s.size()-2] = 0;
-            else if(s[s.size()-2] == '1' && s[s.size()-1] > '0' || s[s.size()-2] == '2' && s[s.size()-1] > '0' && s[s.size()-1] < '7')
-                ways[s.size()-2] = 2;
-            else
-                ways[s.size()-2] = 1;
+        for(int i = s.size() - 1; i >= 0; i--) {
+            if (i == s.size() - 1) {
+                if(decodable(s[i])) {
+                    ways[i] = 1;
+                }
+            } else if (i == s.size() - 2){
+                if (decodable(s[i])) {
+                    ways[i] += ways[i + 1];
+                }
+                string substr = s.substr(i, 2);
+                if (decodable(substr)) {
+                    ways[i]++;
+                }
+            } else {
+                if (decodable(s[i])) {
+                    ways[i] += ways[i + 1];
+                }
+                if (i < s.size() - 2) {
+                    string substr = s.substr(i, 2);
+                    if (decodable(substr)) {
+                        ways[i] += ways[i + 2];
+                    }
+                }
+            }
         }
-        for(int i = s.size()-3; i >=0; i--)
-        {
-            if(s[i+1] == '0' && s[i] > '2')
-                return 0;
-            if(s[i] == '0')
-                ways[i] = 0;
-            else if(s[i+1] > '0' && (s[i] == '1' || s[i] == '2' && s[i+1] < '7'))
-                ways[i] = ways[i+1] + ways[i+2];
-            else if(s[i+1] == '0' && (s[i] == '1' || s[i] == '2'))
-                ways[i] = ways[i+2];
-            else
-                ways[i] = ways[i+1];
-        }
+
         return ways[0];
-    }        
+    }
+
+    bool decodable(char c) {
+        return c != '0';
+    }
+
+    bool decodable(string s) {
+        if (s[0] != '1' && s[0] != '2') {
+            return false;
+        }
+
+        int num = stoi(s);
+        return num <= 26 && num >= 10;
+    }
 };
