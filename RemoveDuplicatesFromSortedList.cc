b28bdc6e5b70c24c283d97d751cdcfc1b75a313e
Yang CHI <yangchi@cisco.com>
2015-02-07 00:34:41 -0800

    better version of Remove Duplicates from Sorted List

--- RemoveDuplicatesFromSortedList.cc
+++ RemoveDuplicatesFromSortedList.cc
@@ -1,60 +1,32 @@
 /**
- ** Definition for singly-linked list.
- ** struct ListNode {
- **     int val;
- **     ListNode *next;
- **     ListNode(int x) : val(x), next(NULL) {}
- ** };
- **/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
-    public:
-        ListNode *deleteDuplicates(ListNode *head) {
-            ListNode *current = head;
-            ListNode *last = NULL;
-            if(!head)
-                return head;
-            if(!head->next)
-                return head;
-            while(current) {
-                if (!last) {
-                    last = current;
-                    current = current->next;
-                } else {
-                    if (current->val == last->val) {
-                        current = current->next;
-                    } else {
-                        if (last->next != current) {
-                            ListNode *temp = last->next;
-                            ListNode *tempNext = temp->next;
-                            while(temp != current) {
-                                delete temp;
-                                temp = tempNext;
-                                if (!temp)
-                                    break;
-                                tempNext = temp->next;
-                            }
-                            last->next = current;
-                            last = current;
-                            current = current->next;
-                        } else {
-                            last = current;
-                            current = current->next;
-                        }
-                    }
-                }
-            }
-            if(last->next != current) {
-                ListNode *temp = last->next;
-                ListNode *tempNext = temp->next;
-                while(temp != current) {
-                    delete temp;
-                    temp = tempNext;
-                    if(!temp)
-                        break;
-                    tempNext = temp->next;
-                }
-                last->next = current;
-            }
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        if (!head || !head->next) {
             return head;
         }
+        ListNode *current = head, *current_next = head->next;
+        while(current_next) {
+            if (current->val == current_next->val) {
+                current->next = current_next->next;
+                delete [] current_next;
+                current_next = current->next;
+                if (!current->next) {
+                    break;
+                }
+            } else {
+                current_next = current_next->next;
+                current = current->next;
+            }
+        }
+
+        return head;
+    }
 };
