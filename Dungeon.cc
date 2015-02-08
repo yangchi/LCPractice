1822cca0f0798b02276d866cd53452eab9e869a9
Yang CHI <yangchi@cisco.com>
2015-02-07 23:54:42 -0800

    another version of Dungeon

--- Dungeon.cc
+++ Dungeon.cc
@@ -1,38 +1,28 @@
 class Solution {
-public:
-    int calculateMinimumHP(vector<vector<int> > &dungeon) {
-        if (dungeon.empty()) {
-            return 1;
-        }
+    public:
+        int calculateMinimumHP(vector<vector<int> > &dungeon) {
+            int rows = dungeon.size();
+            int cols = dungeon[0].size();
+            int dp[rows][cols];
+            int blood;
+            dp[rows - 1][cols - 1] = dungeon[rows - 1][cols - 1] < 0 ? -dungeon[rows - 1][cols - 1] + 1 : 1;
+            for(int i = cols - 2; i >= 0; i--) {
+                int needs = dp[rows - 1][i + 1] - dungeon[rows - 1][i];
+                dp[rows - 1][i] = needs > 0 ? needs : 1;
+            }
 
-        if (dungeon[0].empty()) {
-            return 1;
-        }
-        int rows = dungeon.size();
-        int cols = dungeon[0].size();
-        vector<vector<int>> dp(rows, vector<int>(cols, 0));
-        dp[rows - 1][cols - 1] = dungeon[rows - 1][cols - 1] >= 0 ? 1 : -dungeon[rows - 1][cols - 1] + 1;
+            for(int i = rows - 2; i >= 0; i--) {
+                int needs = dp[i + 1][cols - 1] - dungeon[i][cols - 1];
+                dp[i][cols - 1] = needs > 0 ? needs: 1;
+            }
 
-        for(int i = rows - 1; i >= 0; i--) {
-            for(int j = cols - 1; j >= 0; j--) {
-                int future_needs = INT_MIN;
-                if (i == rows - 1 && j == cols - 1) {
-                    continue;
-                } else if (i == rows - 1) {
-                    future_needs = dp[i][j + 1];
-                } else if (j == cols - 1) {
-                    future_needs = dp[i + 1][j];
-                } else {
-                    future_needs = min(dp[i + 1][j], dp[i][j + 1]);
-                }
-
-                if (dungeon[i][j] >= future_needs) {
-                    dp[i][j] = 1;
-                } else {
-                    dp[i][j] = future_needs - dungeon[i][j];
+            for(int i = rows - 2; i>= 0; i--) {
+                for(int j = cols - 2; j >= 0; j--) {
+                    int needs = -dungeon[i][j] + min(dp[i][j + 1], dp[i + 1][j]);
+                    dp[i][j] = needs > 0 ? needs: 1;
                 }
             }
+
+            return dp[0][0] == 0 ? 1 : dp[0][0];
         }
-        return dp[0][0];
-    }
 };
