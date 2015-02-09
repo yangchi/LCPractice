944ea49db94ab8ddd4398d50f4728249bbc9aca8
Yang CHI <yangchi@cisco.com>
2015-02-09 00:45:21 -0800

    drop the worse combinations

--- combinations.cc
+++ combinations.cc
@@ -7,44 +7,30 @@ using namespace std;
 class Solution {
     public:
         vector<vector<int> > combine(int n, int k) {
-            set<set<int>> sets = combination(n, k);
-            vector<vector<int>> vecs;
-            for(auto eset: sets) {
-                vector<int> vec(eset.begin(), eset.end());
-                vecs.push_back(vec);
-            }
-            return vecs;
-        }
-
-        set<set<int>> combination(int n, int k) {
-            set<set<int>> result;
-            if (k == 0)
-                return result;
-            if (k == 1) {
-                for(int i = 1; i <= n; i++) {
-                    set<int> newset;
-                    newset.insert(i);
-                    result.insert(newset);
-                }
-                return result;
-            }
-            set<set<int>> vecs = combination(n, k - 1);
-            for(auto oldset: vecs) {
-                for(int i = 1; i <= n; i++) {
-                    if(oldset.find(i) == oldset.end()) {
-                        oldset.insert(i);
-                        result.insert(oldset);
-                        oldset.erase(i);
-                    }
-                }
-            }
+            vector<vector<int>> result;
+            vector<int> vec;
+            combination(n, k, 1, vec, result);
             return result;
         }
+
+        void combination(int n, int k, int current, vector<int> & vec, vector<vector<int>> & result) {
+            if (k == 0) {
+                result.push_back(vec);
+                return;
+            }
+
+            for(int i = current; i <= n; i++) {
+                vec.push_back(i);
+                combination(n, k - 1, i + 1, vec, result);
+                vec.pop_back();
+            }
+        }
+
 };
 
 int main() {
     Solution sol;
-    vector<vector<int>> result = sol.combine(6, 4);
+    vector<vector<int>> result = sol.combine(4, 2);
     cout << "size: " << result.size() << endl;
     for(auto vec: result) {
         for(auto elem: vec) {
