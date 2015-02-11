582c51304c12384991162392b7df41981653aad2
Yang CHI <yangchi@cisco.com>
2015-02-10 23:36:34 -0800

    slightly modificatino of Word Break

--- WordBreak.cc
+++ WordBreak.cc
@@ -1,21 +1,19 @@
 class Solution {
     public:
         bool wordBreak(string s, unordered_set<string> &dict) {
-            vector<bool> breakable(s.size(), false);
-            for(int length = 1; length <= s.size(); length++) {
+            vector<bool> valid(s.size() + 1, false);
+            for (int length = 1; length <= s.size(); length++) {
                 if (dict.find(s.substr(0, length)) != dict.end()) {
-                    breakable[length - 1] = true;
-                    continue;
-                }
-
-                for (int breakpoint = 1; breakpoint < length; breakpoint++) {
-                    if (breakable[breakpoint - 1] && dict.find(s.substr(breakpoint, length - breakpoint)) != dict.end()) {
-                        breakable[length - 1] = true;
-                        continue;
+                    valid[length] = true;
+                } else {
+                    for (int cut = 1; cut < length; cut++) {
+                        if (valid[cut] && dict.find(s.substr(cut, length - cut)) != dict.end()) {
+                            valid[length] = true;
+                            break;
+                        }
                     }
                 }
             }
-
-            return breakable[s.size() - 1];
+            return valid[s.size()];
         }
 };
