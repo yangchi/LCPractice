322ac9223525dbc7d299da7f0dcffabf89d0f764
Yang CHI <yangchi@cisco.com>
2015-02-18 15:54:51 -0800

    passed Word break II OJ

--- WordBreakII.cc
+++ WordBreakII.cc
@@ -1,29 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
 class Solution {
     public:
         vector<string> wordBreak(string s, unordered_set<string> &dict) {
-            vector<vector<string>> broken_substr;
-            for(int i = 0; i < s.size(); i++) {
-                broken_substr.push_back(vector<string>());
-            }
-
-            for(int length = 1; length <= s.size(); length++) {
-                string current = s.substr(0, length);
+            vector<vector<string>> broken(s.size(), vector<string>());
+            for (int i = s.size() - 1; i>= 0; i--) {
+                string current = s.substr(i, s.size() - i);
                 if (dict.find(current) != dict.end()) {
-                    broken_substr[length - 1].push_back(current);
+                    broken[i].push_back(current);
                 }
-                for (int breakpoint = 1; breakpoint < length; breakpoint++) {
-                    if (!broken_substr[breakpoint - 1].empty() && dict.find(s.substr(breakpoint, length - breakpoint)) != dict.end()) {
-                        for (auto str : broken_substr[breakpoint - 1]) {
-                            if (str != "") {
-                                broken_substr[length - 1].push_back(str + " " + s.substr(breakpoint, length - breakpoint));
+
+                for (int j = i + 1; j < s.size(); j++) {
+                    string first = s.substr(i, j - i);
+                    string second = s.substr(j, s.size() - j);
+                    if (!broken[j].empty() && dict.find(first) != dict.end()) {
+                        for (auto sub : broken[j]) {
+                            if (sub == "") {
+                                broken[i].push_back(first);
                             } else {
-                                broken_substr[length = 1].push_back(s.substr(breakpoint, length - breakpoint));
+                                broken[i].push_back(first + " " + sub);
                             }
                         }
                     }
                 }
             }
 
-            return broken_substr[s.size() - 1];
+            return broken[0];
         }
 };
+
+int main()
+{
+    Solution sol;
+    unordered_set<string> dict = {"a", "b"};
+    vector<string> res = sol.wordBreak("ab", dict);
+}
